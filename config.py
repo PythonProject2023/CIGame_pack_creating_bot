5b44620a80da822b9e4acdacd9e65491590e7539
Dark-Avery <ingv0rr@yandex.ru>
2023-06-09 21:13:22 +0300

    test redis

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+bot_token = "6202814497:AAH5YxaUT8HzXf3ZomNaySkhwkq0dQDXk0g"
+
+redis_host = os.environ.get("REDISHOST", "localhost")
+redis_port = os.environ.get("REDISPORT", 6379)
+redis_password = os.environ.get("REDISPASSWORD")
